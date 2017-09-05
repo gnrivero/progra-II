e75ab558c1249920dad812964e4b81d3f75a25dd
Gonzalo <grivero@navent.com>
2017-09-05 14:58:42 -0300

    Modificacion de prueba

--- src/TP1/TrabajoPracticoI.java
+++ src/TP1/TrabajoPracticoI.java
@@ -1,9 +1,16 @@
 package TP1;
 
+import impl.ColaPrioridadDA;
+import API.ColaConPrioridadTDA;
+
 public class TrabajoPracticoI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ColaConPrioridadTDA colaPrioridad = new ColaPrioridadDA();
+		
+		colaPrioridad.AcolarPrioridad(1,2);
 
 	}
 
