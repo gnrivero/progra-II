9c6eab1058ba292ca3ce25776e0b7447b903ac01
gnrivero <gnrivero@gmail.com>
2017-12-12 14:18:44 -0300

    Agrego ejercicio 4

--- src/tp3/Ejercicio4.java
+++ src/tp3/Ejercicio4.java
@@ -1,5 +1,11 @@
 package tp3;
 
+import impl.dinamicos.basicos.ColaPrioridadLD;
+import impl.dinamicos.diccionarios.DicMultipleL;
+import impl.estaticos.basicos.ColaPI;
+import api.basicos.ColaConPrioridadTDA;
+import api.diccionarios.DiccionarioMultipleTDA;
+
 public class Ejercicio4 {
 
 	/**
@@ -26,7 +32,33 @@ public class Ejercicio4 {
 	 */
 	public static void main(String[] args) {
 		
-
+		ColaConPrioridadTDA cola = new ColaPrioridadLD();
+		cola.InicializarCola();
+		
+		cola.AcolarPrioridad(10, 5);
+		cola.AcolarPrioridad(10, 10);
+		cola.AcolarPrioridad(9, 14);
+		cola.AcolarPrioridad(6, 1);
+		cola.AcolarPrioridad(5, 1);
+		
+		DiccionarioMultipleTDA diccionario  = pasarColaPaDiccionarioM(cola);
+		
+		System.exit(0);
 	}
-
+	
+	public static DiccionarioMultipleTDA pasarColaPaDiccionarioM(ColaConPrioridadTDA colaPrioridad){
+		
+		
+		DiccionarioMultipleTDA diccionario = new DicMultipleL();
+		diccionario.InicializarDiccionario();
+		
+		
+		while(!colaPrioridad.ColaVacia()){			
+			diccionario.Agregar(colaPrioridad.Prioridad(), colaPrioridad.Primero());
+						
+			colaPrioridad.Desacolar();
+		}
+		
+		return diccionario;
+	}
 }
